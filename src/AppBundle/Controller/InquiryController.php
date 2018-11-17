3a552168a0fe561c9521187137b7d7743a343329
yuta-matsumoto <clock.10.0410@gmail.com>
2018-11-17 14:19:59 +0900

    5章途中

--- src/AppBundle/Controller/InquiryController.php
+++ src/AppBundle/Controller/InquiryController.php
@@ -3,9 +3,85 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
+/**
+ * @Route("/inquiry")
+ */
 class InquiryController extends Controller
 {
+    /**
+     * @Route("/")
+     * @Method("get")
+     */
+    public function indexAction()
+    {
+        return $this->render('Inquiry/index.html.twig',
+                ['form' => $this->createInquiryForm()->createView()]
+        );
+    }
+    
+    /**
+     * @Route("/")
+     * @Method("post")
+     */
+    public function indexPostAction(Request $request)
+    {
+        $form = $this->createInquiryForm();
+        $form->handleRequest($request);
+        if ($form->isValid()){
+            $data = $form->getData();
+            
+            $message = \Swift_Message::newInstance()
+                    ->setSubject('Webサイトからのお問い合わせ')
+                    ->setFrom('webmaster@example.com')
+                    ->setTo('admin@example.com')
+                    ->setBody(
+                            $this->renderView(
+                                    'mail/inquiry.txt.twig',
+                                    ['data' => $data]
+                                    )
+                            );
+            $this->get('mailer')->send($message);
+            
+            return $this->redirect(
+                    $this->generateUrl('app_inquiry_complete'));
+        }
+        
+        return $this->render('Inquiry/index.html.twig',
+                ['form' => $form->createView()]
+        );
+    }
+    
+    /**
+     * @Route("/complete")
+     */
+    public function completeAction()
+    {
+        return $this->render('Inquiry/complete.html.twig');
+    }
+    
+    private function createInquiryForm()
+    {
+        return $this->createFormBuilder()
+                ->add('name', 'text')
+                ->add('email', 'text')
+                ->add('tel', 'text', [
+                    'required' => false,
+                ])
+                ->add('type', 'choice', [
+                    'choices' => [
+                        '公演について',
+                        'その他',
+                    ],
+                    'expanded' => true,
+                ])
+                ->add('content', 'textarea')
+                ->add('submit', 'submit', [
+                    'label' => '送信',
+                ])
+                ->getForm();         
+    }
 }
